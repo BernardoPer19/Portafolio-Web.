e2c68a52606e3f1267a9a3daea50cea707014ec0
jhoaking <wawxper08@gmail.com>
2025-06-06 07:05:05 -1100

    añadiendo modularizacion en el portafolio y mejorando UI

---
+++ src/data/experience.js
@@ -0,0 +1,52 @@
+export const experiences = [
+  {
+    titleProject: "Mi Huella",
+    role: "Frontend Developer, Líder Técnico",
+    company: "LabTecnoSocial",
+    period: "enero 2025 - junio 2025",
+    periodTotal: "6 meses",
+    features: [
+      "Desarrollo de interfaces interactivas con React.",
+      "Implementación de arquitectura escalable para apps empresariales.",
+      "Consumo y gestión eficiente de datos con React Query.",
+      "Organización y liderazgo del equipo frontend.",
+      "Diseño de componentes reutilizables.",
+      "Integración de feedback del usuario para mejorar la UX.",
+      "Documentación técnica y revisión de código colaborativa.",
+    ],
+    description:
+      "Desarrollando interfaces de usuario interactivas con React y soluciones escalables para aplicaciones empresariales.",
+  },
+  {
+    titleProject: "Freelance Web Developer",
+    role: "Fullstack Developer",
+    company: "Proyectos Independientes",
+    period: "2022 - 2023",
+    periodTotal: "1 año",
+    features: [
+      "Creación de sitios web personalizados con HTML, CSS y JavaScript.",
+      "Desarrollo de funcionalidades backend simples con Node.js y PHP.",
+      "Implementación de diseños responsivos para diversos dispositivos.",
+      "Optimización SEO básica para clientes.",
+      "Integración con plataformas como WordPress o correos personalizados.",
+    ],
+    description:
+      "Diseño y desarrollo de sitios web para clientes de diversas industrias, aplicando tecnologías modernas y buenas prácticas.",
+  },
+  {
+    titleProject: "Freelance Web Developer",
+    role: "Fullstack Developer",
+    company: "Proyectos Independientes",
+    period: "2022 - 2023",
+    periodTotal: "1 año",
+    features: [
+      "Creación de sitios web personalizados con HTML, CSS y JavaScript.",
+      "Desarrollo de funcionalidades backend simples con Node.js y PHP.",
+      "Implementación de diseños responsivos para diversos dispositivos.",
+      "Optimización SEO básica para clientes.",
+      "Integración con plataformas como WordPress o correos personalizados.",
+    ],
+    description:
+      "Diseño y desarrollo de sitios web para clientes de diversas industrias, aplicando tecnologías modernas y buenas prácticas.",
+  },
+];
